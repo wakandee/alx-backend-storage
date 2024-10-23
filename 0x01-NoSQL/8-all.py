45d2a062011823884be32bf479ba5d948faf0e79
wakande <koechallanhi@gmail.com>
2024-10-23 15:07:31 -0700

    list all documents in collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+List all documents in a collection Python
+"""
+
+
+def list_all(mongo_collection):
+    """
+    lists all documents in a collection
+
+    :param mongo_collection:
+    :return:
+    """
+    return mongo_collection.find()
+
